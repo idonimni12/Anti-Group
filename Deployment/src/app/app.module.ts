e03752d94f1023df89750060f9e6000aacc21659
ido nimni <ido.nimni12@gmail.com>
2018-03-28 15:55:47 +0300

    added the ui

--- Deployment/src/app/app.module.ts
+++ Deployment/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
 import { PackagesListComponent } from './packages-list/packages-list.component';
+import { FormsModule } from '@angular/forms';
+import { MatGridListModule, MatIconModule, MatRadioModule } from '@angular/material';
 
 
 @NgModule({
@@ -12,7 +12,11 @@ import { PackagesListComponent } from './packages-list/packages-list.component';
     PackagesListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatGridListModule,
+    MatIconModule,
+    FormsModule,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Deployment/src/app/packages-list/packages-list.component.ts
+++ Deployment/src/app/packages-list/packages-list.component.ts
@@ -1,5 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+interface IPackage {
+  cols: number;
+  rows: number;
+  img: string;
+  id: string;
+  name: string;
+  version: string;
+  picked: boolean;
+}
+
+interface IPackageList {
+  packages: IPackage[];
+  type: string;
+}
+
 @Component({
   selector: 'app-packages-list',
   templateUrl: './packages-list.component.html',
@@ -7,9 +22,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PackagesListComponent implements OnInit {
 
-  constructor() { }
+  packages: IPackage[];
+  deployTo: string;
+  options = [
+    'Salt',
+    'Container'
+  ];
+  constructor() {
+    this.packages = [
+      {
+        name: 'python',
+        cols: 1,
+        rows: 1,
+        img: '../../assets/python.png',
+        id: '',
+        version: '2.7',
+        picked: false
+      },
+      {
+        name: 'mongoDB',
+        cols: 1,
+        rows: 1,
+        img: '../../assets/mongodb.png',
+        id: '',
+        version: '3.6',
+        picked: false
+      }
+    ];
+
+  }
+
+  onPick(pack: IPackage) {
+    for (let i = 0; i < this.packages.length; i++) {
+      if (this.packages[i].name === pack.name) {
+        this.packages[i].picked = !this.packages[i].picked;
+      }
+
+    }
+  }
+
+  onGenerateClick() {
+    const pickedPackges: IPackage[] = [];
+    for (let i = 0; i < this.packages.length; i++) {
+      if (this.packages[i].picked) {
+        pickedPackges.push(this.packages[i]);
+      }
+    }
+    const packageList: IPackageList = {packages: pickedPackges, type: this.deployTo};
+    console.log(packageList);
+    // generatorRouting(packageList)
+  }
+
+
+  fileChange(event) {
+    const fileList: FileList = event.target.files;
+    if (fileList.length > 0) {
+        const file: File = fileList[0];
+        const formData: FormData = new FormData();
+        formData.append('uploadFile', file, file.name);
+    }
+  }
 
   ngOnInit() {
+
   }
 
 }
