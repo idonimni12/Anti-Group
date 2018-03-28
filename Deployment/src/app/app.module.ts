0d986e44ac6de2a0583da0660a2e01a18ddc9512
ido nimni <ido.nimni12@gmail.com>
2018-03-28 10:22:44 +0300

    changed the package json -> and added packeges list componnent

--- Deployment/src/app/app.module.ts
+++ Deployment/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PackagesListComponent } from './packages-list/packages-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PackagesListComponent
   ],
   imports: [
     BrowserModule
