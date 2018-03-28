1bc540901a0709ba8b2313aec42a96e766489984
ZaRa <travian014@walla.com>
2018-03-28 16:02:44 +0300

    Added generator.

---
+++ Deployment/src/app/generator/generator.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-generator',
+  templateUrl: './generator.component.html',
+  styleUrls: ['./generator.component.css']
+})
+export class GeneratorComponent implements OnInit {
+  private masterServer = '130.61.93.213';
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    function generatorRouting(packageList) {
+      packageList = {server: 'ez', packages: [{'_id': 1, 'name': 'abc'}, {'_id': 2, 'name': 'lol'}], type: 'salt'};
+      if (packageList['type'] == 'docker') {
+        console.log('Generating Docker file...');
+        generateDockerFile(packageList['packages']);
+      }
+      else if (packageList['type'] == 'salt'){
+        console.log('Generating Salt file...');
+        generateSaltFile(packageList['packages']);
+      }
+      else {
+        console.log('Type not supported')
+      }
+    }
+
+    function readFile(file){
+      let raw = new XMLHttpRequest(); // create a request
+      raw.open("GET", file, false); // open file
+      raw.onreadystatechange = function () { // file is ready to read
+        if (raw.readyState === 4) {
+          if (raw.status === 200 || raw.status == 0) {
+            let allText = raw.responseText;
+            alert(allText); // can be also console.logged, of course.
+          }
+        }
+      };
+      raw.send(null); // return control
+    }
+
+    function generateSaltFile(packageList) {
+      let saltFileTemplate = readFile('../files/install_package.txt');
+      //packageList.forEach(pkg => saltFileTemplate += readFile('app/files/install_package.txt') + pkg['name']);
+      //console.log(saltFileTemplate);
+    }
+
+    function generateDockerFile(packageList) {
+
+    }
+
+
+    generatorRouting(2);
+  }
+}
