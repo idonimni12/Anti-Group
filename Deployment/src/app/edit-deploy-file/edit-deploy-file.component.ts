aa1ef2b8bb79df772f5e742d4f4b4a9fa7ec5fbd
ido nimni <ido.nimni12@gmail.com>
2018-03-29 05:22:53 +0300

    edit

--- Deployment/src/app/edit-deploy-file/edit-deploy-file.component.ts
+++ Deployment/src/app/edit-deploy-file/edit-deploy-file.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output } from '@angular/core';
+import {HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-edit-deploy-file',
@@ -11,8 +12,9 @@ export class EditDeployFileComponent implements OnInit {
   @Input() value: string;
   @Input() type: string;
   loading: boolean;
+  server = 'http://130.61.93.213:3000/upload';
 
-  constructor() {
+  constructor(private http: HttpClient) {
     this.loading = false;
    }
 
@@ -21,6 +23,13 @@ export class EditDeployFileComponent implements OnInit {
     setTimeout(() => {
       this.loading = false;
     }, 3000);
+    this.http.post(this.server, this.value).subscribe(
+      res => {
+        console.log(res);
+      }, err => {
+        console.log(err);
+    });
+
   }
 
   ngOnInit() {
